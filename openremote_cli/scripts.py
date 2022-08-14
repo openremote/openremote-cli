a524f8417d6f24a6b08d97b7706e5be32b6eb824
Michal Rutka <michal.rutka@gmail.com>
2022-08-14 18:07:55 +0200

    Remove keypair check

--- openremote_cli/scripts.py
+++ openremote_cli/scripts.py
@@ -387,13 +387,6 @@ def check_aws_perquisites():
     if not config.DRY_RUN and config.PROFILE not in output:
         msg = f"aws-cli profile '{config.PROFILE}' missing"
         raise Exception(-1, msg)
-    # Check EC2 key
-    code, output = shell.execute(
-        f'aws ec2 describe-key-pairs --key-names openremote --profile {config.PROFILE}'
-    )
-    if not config.DRY_RUN and 'openremote' not in output:
-        msg = f"ERROR: Missing EC2 keypair 'openremote' in {config.REGION} region (Ireland)"
-        raise Exception(-1, msg)
     return True
 
 
